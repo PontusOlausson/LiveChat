451346ff1c04d8ccee341e7feb8d55e37408312a
PontusOlausson <pontusolausson@hotmail.com>
2016-06-14 21:59:15 +0200

    Started Server cide

--- LiveChat - Server/LiveChat - Server/Program.cs
+++ LiveChat - Server/LiveChat - Server/Program.cs
@@ -3,13 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
 
 namespace LiveChat___Server
 {
     class Program
     {
+        public static Hashtable clientsList = new Hashtable();
+
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Any;
+            int port = 8888;
+            TcpListener serverSocket = new TcpListener(ip, port);
+            TcpClient clientSocket = default(TcpClient);
+
+            serverSocket.Start();
+            Console.WriteLine("Chat Server Started ....");
+            Console.WriteLine("IP address: " + ip);
+            Console.WriteLine("Port: " + port);
+
+            while (true)
+            {
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                //networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                clientsList.Add(dataFromClient, clientSocket);
+
+                Broadcast(dataFromClient + " Joined ", dataFromClient, false);
+
+                Console.WriteLine(dataFromClient + " Joined chat room ");
+                HandleClient client = new HandleClient();
+                client.StartClient(clientSocket, dataFromClient, clientsList);
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine("exit");
+            Console.ReadLine();
+        }
+
+        public static void Broadcast(string message, string uName, bool flag)
+        {
+            foreach (DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+
+                if (flag == true)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says : " + message);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(message);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }
+    }
+
+    public class HandleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        Hashtable clientsList;
+
+        public void StartClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            this.clientsList = cList;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    //networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    networkStream.Read(bytesFrom, 0, 10025);
+
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine("From client - " + clNo + " : " + dataFromClient);
+                    rCount = Convert.ToString(requestCount);
+
+                    Program.Broadcast(dataFromClient, clNo, true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
         }
     }
 }
